afd83da5bea399e5c8a124836d3388f14c896efa
Ngoc <ngocpham180404@gmail.com>
2021-05-27 15:36:59 +0700

    first commit

---
+++ 1-name.js
@@ -0,0 +1,8 @@
+//local info
+const secret = "SECRET";
+//sharing info
+const john = "john";
+const peter = "peter";
+
+module.exports = {john, peter};
+module.exports.items = ['item1', 'item2'];
---
+++ 2-utility.js
@@ -0,0 +1,8 @@
+const num1 = 5;
+const num2 = 10;
+
+function sum() {
+  console.log(`The result is: ${num1 + num2}`);
+}
+
+sum();
